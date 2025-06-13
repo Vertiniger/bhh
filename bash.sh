a5dcba2e0a2027178ff97119e60c350c4f1ae9a1
Vertiniger <yudasuhada29@gmail.com>
2025-06-13 21:28:49 +0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -8,8 +8,11 @@ OUTPUT_FILE="active.txt"
 while IFS= read -r proxy; do
     [[ -z "$proxy" ]] && continue
 
-    status_code=$(curl -s -o /dev/null -w "%{http_code}" --proxy "http://$proxy" -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36
-" http://httpbin.org/ip)
+    status_code=$(curl -s -o /dev/null -w "%{http_code}" \
+        --proxy "http://$proxy" \
+        -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36" \
+        --max-time 0.002 \
+        http://httpbin.org/ip)
 
     if [[ "$status_code" != "403" && "$status_code" != "000" ]]; then
         echo "[ACTIVE] $proxy"
