0d533135a8518704d1bdba3b29dd8565e40244e2
Vertiniger <yudasuhada29@gmail.com>
2025-06-13 21:21:34 +0700

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+INPUT_FILE="active_proxies.txt"
+OUTPUT_FILE="active.txt"
+
+> "$OUTPUT_FILE"
+
+while IFS= read -r proxy; do
+    [[ -z "$proxy" ]] && continue
+
+    status_code=$(curl -s -o /dev/null -w "%{http_code}" --proxy "http://$proxy" -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36
+" http://httpbin.org/ip)
+
+    if [[ "$status_code" != "403" && "$status_code" != "000" ]]; then
+        echo "[ACTIVE] $proxy"
+        echo "$proxy" >> "$OUTPUT_FILE"
+    fi
+done < "$INPUT_FILE"
+
+echo "[DONE] Proxy checking completed. Results in $OUTPUT_FILE"
